6ff99adf4f8005352e30bd188d10d2dd524b204c
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-09-15 14:37:13 +0200

    basic updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,14 @@
+const now = String(Date.now())
+
 module.exports = function(eleventyConfig) {
+    
+    eleventyConfig.addPassthroughCopy({'./node_modules/sal.js/dist/sal.js': './js/sal.js',})
+    eleventyConfig.addPassthroughCopy({'./node_modules/sal.js/dist/sal.css': './sal.css',})
+
+    eleventyConfig.addShortcode('version', function () {
+        return now
+    })
+
     eleventyConfig.addPassthroughCopy("src/img");
     return {
         dir: {
