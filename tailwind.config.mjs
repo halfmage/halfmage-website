82d4ef6bc9570ee88a19178cf7d37085b48b425f
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-10-29 09:43:58 +0100

    update for astro

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		container: {
+			center: true,
+			padding: '1rem'
+		},
+		extend: {},
+	},
+	plugins: [],
+}
